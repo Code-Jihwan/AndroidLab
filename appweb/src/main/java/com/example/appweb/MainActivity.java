ba231b645a223e6ad1667e3dff99a9cf3505c833
jihwan <jihwan3194@naver.com>
2022-08-01 23:10:52 +0900

    aa

--- appweb/src/main/java/com/example/appweb/MainActivity.java
+++ appweb/src/main/java/com/example/appweb/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.appweb;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();    //타이틀바 제거(숨기기)
+        actionBar.hide();
     }
 }
